e54f6a4d0c6d410c0bc5aea1640c29ae7d1dbe2d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-11-17 14:36:56 +0100

    Removed unused test

--- lib/page_downloader/url_resolver.rb
+++ lib/page_downloader/url_resolver.rb
@@ -7,7 +7,6 @@ class UrlResolver
     normal_url(given_url) ||
       url_for_double_dashed(given_url) ||
       slash_based_url(given_url) ||
-      non_prefixed_url(given_url) ||
       given_url
   end
 
@@ -41,12 +40,5 @@ class UrlResolver
   def normal_url(url)
     is_url = base_url_for(url)
     url if is_url
-  end
-
-  def non_prefixed_url(url)
-    expanded = File.expand_path(url, referer)
-    expanded = expanded.split("http")
-    expanded.shift
-    expanded.unshift("").join("http")
   end
 end
